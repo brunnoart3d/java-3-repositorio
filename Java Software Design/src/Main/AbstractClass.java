cf84222e796a0b1674aece1f9bcc7f8b8d3dd3f4
brunnoart3d <brunnoart3d@gmail.com>
2024-08-10 01:40:19 -0300

    Update AbstractClass.java

--- Java Software Design/src/Main/AbstractClass.java
+++ Java Software Design/src/Main/AbstractClass.java
@@ -7,24 +7,26 @@ public abstract class AbstractClass implements Interface1 {
     public int password;
 
     // constructors
-    public AbstractClass()
+    public AbstractClass() // método não abstrato
 
     {
         password = 9999;
     }
 
-    public AbstractClass(int password)
+    public AbstractClass(int password) // método não abstrato
 
     {
         this.password = password;
     }
 
     // behaviours
-    public void sayHello() {
+    public void sayHello()
+
+    {
         System.out.println("Olá");
     }
 
-    public abstract void sayHi();// não pode ser implementado na classe abstrata
+    public abstract void sayHi();// //método abstrato! não pode ser implementado na classe abstrata
 
     @Override
     public void interfaceMothod2() {
